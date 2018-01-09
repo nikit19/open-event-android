0fd9a92a40d161aeda91a71192b10eebe53ae1bd
nikit19 <nikit.bhandari@gmail.com>
2018-01-09 16:50:01 +0530

    added realm data wrapper

--- android/app/src/main/java/org/fossasia/openevent/viewmodels/TracksFragmentViewModel.java
+++ android/app/src/main/java/org/fossasia/openevent/viewmodels/TracksFragmentViewModel.java
@@ -2,65 +2,67 @@ package org.fossasia.openevent.viewmodels;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Transformations;
 import android.arch.lifecycle.ViewModel;
 
 import org.fossasia.openevent.data.Track;
+import org.fossasia.openevent.dbutils.LiveRealmData;
 import org.fossasia.openevent.dbutils.RealmDataRepository;
 
 import java.util.List;
 import java.util.Locale;
 
 import io.reactivex.Observable;
-import io.realm.RealmResults;
 import timber.log.Timber;
 
 public class TracksFragmentViewModel extends ViewModel {
 
-    private MutableLiveData<List<Track>> tracksList;
-    private MutableLiveData<List<Track>> filteredTracksList;
+    private LiveData<List<Track>> tracksList;
+    private LiveData<List<Track>> filteredTracksList;
 
     private RealmDataRepository realmRepo;
-    private RealmResults<Track> realmResults;
 
     private String searchText = "";
 
     public TracksFragmentViewModel() {
         realmRepo = RealmDataRepository.getDefaultInstance();
+        tracksList = new MutableLiveData<>();
+        filteredTracksList = new MutableLiveData<>();
+        subscribeToLocations();
+
     }
 
     public LiveData<List<Track>> getTracks(String searchText) {
         setSearchText(searchText);
-        if (filteredTracksList == null)
-            filteredTracksList = new MutableLiveData<>();
-        if (tracksList == null) {
-            tracksList = new MutableLiveData<>();
-            realmResults = realmRepo.getTracks();
-            realmResults.addChangeListener((tracks, orderedCollectionChangeSet) -> {
-                tracksList.setValue(tracks);
-                getFilteredTracks();
-            });
-        } else getFilteredTracks();
+        getFilteredTracks();
 
         return filteredTracksList;
+    }
+
+    private void subscribeToLocations() {
+        LiveRealmData<Track> tracksLiveRealmData = RealmDataRepository.asLiveData(realmRepo.getTracks());
+        tracksList = Transformations.map(tracksLiveRealmData, input -> input);
     }
 
     private void getFilteredTracks() {
 
         final String query = searchText.toLowerCase(Locale.getDefault());
 
-        Observable.fromIterable(tracksList.getValue())
-                .filter(track -> track.getName()
-                        .toLowerCase(Locale.getDefault())
-                        .contains(query))
-                .toList().subscribe(trackList -> {
-            filteredTracksList.setValue(trackList);
+        if (tracksList.getValue() != null) {
+            Observable.fromIterable(tracksList.getValue())
+                    .filter(track -> track.getName()
+                            .toLowerCase(Locale.getDefault())
+                            .contains(query))
+                    .toList().subscribe(trackList -> {
+                filteredTracksList = Transformations.map(tracksList, input -> input);
 
-            Timber.d("Filtering done total results %d", trackList.size());
+                Timber.d("Filtering done total results %d", trackList.size());
 
-            if (trackList.isEmpty()) {
-                Timber.e("No results published. There is an error in query. Check " + getClass().getName() + " filter!");
-            }
-        });
+                if (trackList.isEmpty()) {
+                    Timber.e("No results published. There is an error in query. Check " + getClass().getName() + " filter!");
+                }
+            });
+        }
     }
 
     public String getSearchText() {
@@ -73,7 +75,6 @@ public class TracksFragmentViewModel extends ViewModel {
 
     @Override
     protected void onCleared() {
-        realmResults.removeAllChangeListeners();
         super.onCleared();
     }
 
